99cc738a4fcf4a1894cf0cb297d8eabf281c5988
Aayush Gour <138443486+itsaayush27@users.noreply.github.com>
2025-08-31 15:42:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function makeid(l) {
   // write your code here
+	var res  =" ";
+	var char_list ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*";
+
+	for(var i = 0; i<l; i++){
+			res += char_list.charAt(
+				Math.floor(
+					Math.random()*char_list.length
+				)
+				
+			)
+	}
+	return res;
 }
+
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
